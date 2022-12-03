0ad2ef8612f2d3c8948ee5f3e8446c62587a549b
Valentyn Blaha <valentyn.blaha@gmail.com>
2022-12-03 15:48:28 +0100

    get_comments v2

--- scrape.py
+++ scrape.py
@@ -1,42 +1,52 @@
 from googleapiclient.discovery import build
+import json
 
 api_key = 'AIzaSyD-u-cmOMhu0jxnOme5mizTWGQHzoM0X8c'
 
 youtube = build('youtube', 'v3', developerKey = api_key)
 
+def comment_to_dict(comment_snippet):
+    return {
+        'author':   comment_snippet['authorDisplayName'],
+        'text':     comment_snippet['textDisplay'],
+        'likes':    comment_snippet['likeCount']
+    }
+
 def get_comments(videoID):
     
     comments = []
 
+    def get_request(page_token):
+        return youtube.commentThreads().list(
+            part        = 'snippet,replies',
+            videoId     = videoID,
+            order       = 'time', # Time retrieves data faster compared to relevance (default)
+            maxResults  = 100,
+            pageToken   = page_token,
+            textFormat  = 'plainText'
+        )
+
     def append_from_response(response):
         for item in response['items']:
-            comments.append(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+            comments.append(comment_to_dict(item['snippet']['topLevelComment']['snippet']))
             # Add replies to that comment as well
             if item.get('replies'):
                 for reply in item['replies']['comments']:
-                    comments.append(reply['snippet']['textDisplay'])
+                    comments.append(comment_to_dict(reply['snippet']))
 
-    request = youtube.commentThreads().list(
-        part='snippet,replies',
-        videoId=videoID,
-        order='time',
-        maxResults=100
-    )
+    request = get_request(None)
     response = request.execute()
     append_from_response(response)
     while response.get('nextPageToken'):
-        request = youtube.commentThreads().list(
-            part='snippet,replies',
-            videoId=videoID,
-            order='time',
-            pageToken = response['nextPageToken'],
-            maxResults=100
-        )
+        request = get_request(response['nextPageToken'])
         response = request.execute()
         append_from_response(response)
     
     return comments
 
 # WARNING: executing this will reduce the available points for further uses of the API
-comments = get_comments('rfscVS0vtbw')
-print(len(get_comments('rfscVS0vtbw')))
+comments = get_comments('Yxg96Re2X7k')
+f = open('Yxg96Re2X7k.json', 'w')
+f.write(json.dumps(comments))
+f.close()
+print(len(comments))
