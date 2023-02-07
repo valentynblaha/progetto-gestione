17eb62813174c0a3aae5de4ea1baf88fd3b01f24
Valentyn Blaha <valentyn.blaha@gmail.com>
2023-02-07 17:28:56 +0100

    Added queries

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ import json
 
 searcher = VideoSearcher('indexdir')
 
-results = searcher.search(searcher.parse_query('html'))
+results = searcher.search(searcher.parse_query('kind:video AND solidity AND publishedAt:[7 feb 2021 TO]'))
 
 print(f'Found {results.scored_length()} results')
 for result in results:
